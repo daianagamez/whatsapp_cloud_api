94a3f92ef8136dc816f68fb9f3a462812e1e8558
daianagamez <daianaigv@gmail.com>
2024-12-20 16:59:58 -0400

    [ADD] options menu and response management

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -6,11 +6,17 @@ class MessageHandler {
         const incomingMessage = message.text.body.toLowerCase().trim();
 
         if(this.isGreeting(incomingMessage)){
-            await this.sendWelcomeMessage(message.from, message.id, senderInfo)
+            await this.sendWelcomeMessage(message.from, message.id, senderInfo);
+            await this.sendWelcomeMenu(message.from)
         }else{
             const response = `Probandoooo: ${message.text.body}`;
             await whatsappService.sendMessage(message.from, response, message.id);
         }
+        await whatsappService.markAsRead(message.id);
+    }else if(message?.type === 'interactive'){
+        //console.log("message?.interactive?: ", message?.interactive)
+        const option = message?.interactive?.button_reply?.id
+        await this.handlerMenuOption(message.from, option)
         await whatsappService.markAsRead(message.id);
     }
   }
@@ -24,6 +30,7 @@ class MessageHandler {
   getSenderName(senderInfo){
     return senderInfo.profile?.name || senderInfo.wa_id || "";
   }
+
   nameFormat(name){
     //Eliminar emojis y caracteres especiales no alfabeticos
     const nameSinEmojis = name.replace(/[^a-zA-Z\s]/g, '');
@@ -35,6 +42,7 @@ class MessageHandler {
     //Validación con IA o en su defecto pedir directamente que devuelva el nombre
   }
 
+  //Funcion para enviar mensaje de bienvenida
   async sendWelcomeMessage(to, messageId, senderInfo){
     console.log ("senderInfo: ", senderInfo);
     let name = this.getSenderName(senderInfo);
@@ -43,5 +51,41 @@ class MessageHandler {
     await whatsappService.sendMessage(to, welcomeMessage, messageId)
   }
 
+  //Envio de menu inicial al usuario
+  async sendWelcomeMenu(to){
+    const menuMessage = "Elige una opción por favor"
+    const buttons = [
+        {
+            type: 'reply', reply: {id: 'option1', title: 'Arreglos'}
+        },
+        {
+            type: 'reply', reply: {id:'option2', title: 'Postres'}
+        },
+        {
+            type: 'reply', reply: {id:'option3', title: 'Ubicación'}
+        }
+    ];
+    await whatsappService.sendInteractiveButtons(to, menuMessage, buttons)
+  }
+
+
+  //Switch menu de opciones
+  async handlerMenuOption(to, option){
+    let response;
+    switch(option){
+        case "option1":
+            response = "Solicitar arreglo"
+            break;
+        case "option2":
+            response = "Pedir Postre"
+            break;
+        case "option3":
+            response = "Esta es mi Ubicación"
+            break;
+        default:
+            response = "Lo siento, no entendi tu seleccón. por favor, elige una de las opciones"
+    }
+    await whatsappService.sendMessage(to, response)
+  }
 }
 export default new MessageHandler();
--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -15,9 +15,9 @@ class WhatsAppService {
             messaging_product: 'whatsapp',
             to,
             text: { body },
-            context: {
+            /*context: { //Esto es para replicar el mensaje/responder sobre el msm
                 message_id: messageId,
-            },
+            },*/
             },
         });
         } catch (error) {
@@ -41,6 +41,33 @@ class WhatsAppService {
         });
         } catch (error) {
         console.error('Error marking message as read:', error);
+        }
+    }
+    //Enviar mensajes tipo boton
+    async sendInteractiveButtons(to, bodyText, buttons){
+        try{
+            await axios({
+                method: 'POST',
+                url: `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`,
+                headers: {
+                  Authorization: `Bearer ${config.API_TOKEN}`,
+                },
+                data: {
+                  messaging_product: 'whatsapp',
+                  to,
+                  type: 'interactive',
+                  interactive: {
+                    type: 'button',
+                    body: { text: bodyText },
+                    action: {
+                      buttons: buttons
+                    }
+                  }
+                },
+            });
+
+        }catch(e){
+            console.error("Error al enviar msm button: ", e)
         }
     }
 }
