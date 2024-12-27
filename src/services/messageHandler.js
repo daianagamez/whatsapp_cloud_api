a56bfaf2e04be704e191afcc0265b88b45cbe11e
daianagamez <daianaigv@gmail.com>
2024-12-27 17:06:04 -0400

    [ADD] handleOrderFlow

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -1,5 +1,10 @@
 import whatsappService from './whatsappService.js';
 class MessageHandler {
+
+  constructor(){
+    this.orderState ={};
+  }
+
   async handleIncomingMessage(message, senderInfo) {
     if (message?.type === 'text') {
         //Validación de Saludo
@@ -10,6 +15,8 @@ class MessageHandler {
             await this.sendWelcomeMenu(message.from)
         } else if(this.isMediaType(incomingMessage)){
           await this.sendMedia(message.from, incomingMessage)
+        } else if(this.orderState[message.from]){
+          await this.handleOrderFlow(message.from, incomingMessage)
         }
         else{
             const response = `Probandoooo: ${message.text.body}`;
@@ -64,14 +71,28 @@ class MessageHandler {
     const menuMessage = "Elige una opción por favor"
     const buttons = [
         {
-            type: 'reply', reply: {id: 'option1', title: 'Arreglos'}
+            type: 'reply', reply: {id: 'option1', title: 'Catálogo'}
         },
         {
-            type: 'reply', reply: {id:'option2', title: 'Postres'}
+            type: 'reply', reply: {id:'option2', title: 'Realizar Pedido'}
         },
         {
             type: 'reply', reply: {id:'option3', title: 'Ubicación'}
         }
+    ];
+    await whatsappService.sendInteractiveButtons(to, menuMessage, buttons)
+  }
+
+  //order options
+  async sendOrderOption(to){
+    const menuMessage = "¿Qué tipo de pedido deseas realizar?"
+    const buttons = [
+        {
+            type: 'reply', reply: {id: 'order_option1', title: 'Arreglos'}
+        },
+        {
+            type: 'reply', reply: {id:'order_option2', title: 'Postres'}
+        },
     ];
     await whatsappService.sendInteractiveButtons(to, menuMessage, buttons)
   }
@@ -82,14 +103,19 @@ class MessageHandler {
     let response;
     switch(option){
         case "option1":
-            response = "Solicitar arreglo"
+            response = "Catálogo"
             break;
         case "option2":
-            response = "Pedir Postre"
-            break;
+            await this.sendOrderOption(to)
+            return;
         case "option3":
             response = "Esta es mi Ubicación"
             break;
+        case "order_option1":
+          this.orderState[to] = {step: "name"};
+          response = "Por favor, ingresa tu nombre: "
+          console
+          break;
         default:
             response = "Lo siento, no entendi tu seleccón. por favor, elige una de las opciones"
     }
@@ -127,6 +153,38 @@ class MessageHandler {
     }catch(e){
       console.error("Error in SendMedia: ", e)
     }
-}
+  }
+
+  async handleOrderFlow(to, message){
+    const state = this.orderState[to];
+    let response;
+
+    switch(state.step){
+      case "name":
+        state.name = message
+        state.step = "orderCategory"
+        response = "Gracias, ¿Podrías indicarnos el tipo de arreglo que te interesa? Tenemos opciones como: Flores con Chocolates, Frutas Decoradas, y Dulces Temáticos. Por favor, escribe el nombre del arreglo"
+        break;
+      case "orderCategory":
+        state.orderCategory = message
+        state.step = "orderDetails"
+        response = "¡Genial! ¿Tienes alguna preferencia de color o algún mensaje especial que quieras incluir? Si es así, por favor detállalo."
+        break;
+      case "orderDetails":
+        state.petType = message
+        state.step = "deliveryDetails"
+        response = "Ahora, por favor indícanos la fecha y dirección de entrega"
+        break;
+      case "deliveryDetails":
+        state.petType = message
+        response = "Gracias por proporcionarnos toda la información. Nos pondremos en contacto contigo para confirmar los detalles del pago y la entrega."
+        delete this.orderState[to];
+        break;
+      default:
+        break;
+    }
+
+    await whatsappService.sendMessage(to, response)
+  }
 }
 export default new MessageHandler();
